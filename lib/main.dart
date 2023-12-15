4e311d51a319a2cba8a35a54c262793122ece17d
sachindu kavishka <cst21017@std.uwu.ac.lk>
2023-12-15 13:47:12 +0530

    MyCommitment

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/QrScanner.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Qr Code scanner",
+      home: const QrScanner(),
+      theme: ThemeData(
+        colorScheme: const ColorScheme.dark(),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent
+        )
+      ),
+    );
+  }
+}
